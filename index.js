a8e6c9d0efba09d6dff1647894e68c53062e6fc9
gurelbs <guri240@gmail.com>
2021-04-14 19:47:46 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.listen(PORT, () => console.log(`server run at http://localhost:${PORT}`))
