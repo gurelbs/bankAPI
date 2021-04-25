e041290f7aebf20616060f6c6a0f40a09121ee7b
gurelbs <guri240@gmail.com>
2021-04-25 08:51:53 +0300

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000
 const app = express()
 const userRouter = require('./routers/user')
 const accountRouter = require('./routers/account')
-// app.get('/',express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'client/build')));
 // app.use(express.static(path.join(__dirname, 'public')))
 app.use(cors());
 app.use(express.json())
