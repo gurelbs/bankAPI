5311cdf7133d7e4dda09524803ec2e990c478065
gurelbs <guri240@gmail.com>
2021-04-26 22:09:56 +0300

    update

--- client/src/components/UserPage.jsx
+++ client/src/components/UserPage.jsx
@@ -25,9 +25,9 @@ function UserPage() {
                     : process.env.NODE_ENV === 'production'
                     ? `/user/${id}`
                     : null
-                    const {data} = await api.get(pathname, {cancelToken: source.token})
-                    setUserData(data)
-                    console.log(id,data)
+                const {data} = await api.get(pathname, {cancelToken: source.token})
+                setUserData(data)
+                console.log(id,data,pathname)
                 setSpinner(false)
                 setTimeout(() => {
                     setGetMsg('')
@@ -59,8 +59,13 @@ function UserPage() {
     const handleCreateAccount = () => {
         const fetchData = async () => {
             try {
-                const pathname = '/account/create'
-                const {data} = await api.post(pathname, {owner: userData._id})
+                const pathname = 
+                    process.env.NODE_ENV === 'development' 
+                    ? 'account/create' 
+                    : process.env.NODE_ENV === 'production'
+                    ? 'account/create'
+                    : null
+                const {data} = await api.post(pathname, {owner: id})
                 setCreateAccountMsg(data)
             } catch (e) {
                 console.log(e);
--- routers/account.js
+++ routers/account.js
@@ -11,13 +11,17 @@ router.post('/api/account/create', async (req,res) => {
     try {
         const user = await User.findById(req.body.owner).exec()
         if (!user) res.status(404).json('i can`t find this account owner...')
-        User.findOneAndUpdate({_id: user._id}, 
+        await User.findOneAndUpdate({_id: user._id}, 
             {"$push": {"accounts": newAcount._id}},
             { "new": true, "upsert": true }, (err, user) => {
                 if (err) res.status(500).json('cant update user...')
                 newAcount.save( async (err, account) => {
-                    if (err) res.status(500).json('cant update account...')
-                    res.status(201).json(`hey ${user.name}, your account created!. this is your accout number: ${newAcount._id}`)
+                    try {
+                        if (err) res.status(500).json('cant update account...')
+                        res.status(201).json(`hey ${user.name}, your account created!. this is your accout number: ${newAcount._id}`)
+                    } catch (e) {
+                        res.status(500).json(`there is some error ${e}`)
+                    }
                 })
             })
     } catch (e) {
