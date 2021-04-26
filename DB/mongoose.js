b7bcc0a8ccd5ce0be56553d6d834ded40f999e08
gurelbs <guri240@gmail.com>
2021-04-27 01:33:39 +0300

    update

--- DB/mongoose.js
+++ DB/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require('dotenv').config();
 const uri = process.env.ATLAS_URI
 const connection = mongoose.connection 
-mongoose.connect('mongodb+srv://gurel:DaBy18emmxKg6AkI@cluster0.qmznj.mongodb.net/BANKAPI?retryWrites=true&w=majority', {
+mongoose.connect(uri, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
